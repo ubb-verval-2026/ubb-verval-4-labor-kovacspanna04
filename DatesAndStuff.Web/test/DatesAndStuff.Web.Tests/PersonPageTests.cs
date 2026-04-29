068ca0c5b1ac2ec8d1d75bb26646a9ea6f60b60c
kovacspanna04 <kovacspanna3@gmail.com>
2026-04-29 16:24:29 +0300

    1. done

--- DatesAndStuff.Web/test/DatesAndStuff.Web.Tests/PersonPageTests.cs
+++ DatesAndStuff.Web/test/DatesAndStuff.Web.Tests/PersonPageTests.cs
@@ -97,29 +97,39 @@ public class PersonPageTests
         Assert.That(verificationErrors.ToString(), Is.EqualTo(""));
     }
 
-    [Test]
-    public void Person_SalaryIncrease_ShouldIncrease()
+    [TestCase(5)]
+    [TestCase(10)]
+    [TestCase(0)]
+    [TestCase(-5)]
+    [TestCase(20)]
+    public void Person_SalaryIncrease_ShouldIncrease(double percentage)
     {
         // Arrange
         driver.Navigate().GoToUrl(BaseURL);
         driver.FindElement(By.XPath("//*[@data-test='PersonPageNavigation']")).Click();
 
-        var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+        var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
 
-        var input = wait.Until(ExpectedConditions.ElementExists(By.XPath("//*[@data-test='SalaryIncreasePercentageInput']")));
+        wait.Until(ExpectedConditions.ElementToBeClickable(
+            By.XPath("//*[@data-test='PersonPageNavigation']"))).Click();
+
+        var input = wait.Until(ExpectedConditions.ElementToBeClickable(
+            By.XPath("//*[@data-test='SalaryIncreasePercentageInput']")));
+
         input.Clear();
-        input.SendKeys("5");
+        //input.SendKeys("5");
+        input.SendKeys(percentage.ToString(System.Globalization.CultureInfo.InvariantCulture));
 
-        // Act
-        var submitButton = wait.Until(ExpectedConditions.ElementExists(By.XPath("//*[@data-test='SalaryIncreaseSubmitButton']")));
-        submitButton.Click();
-
+        wait.Until(ExpectedConditions.ElementToBeClickable(
+            By.XPath("//*[@data-test='SalaryIncreaseSubmitButton']"))).Click();
 
         // Assert
         var salaryLabel = wait.Until(ExpectedConditions.ElementExists(By.XPath("//*[@data-test='DisplayedSalary']")));
-        var salaryAfterSubmission = double.Parse(salaryLabel.Text);
-        salaryAfterSubmission.Should().BeApproximately(5250, 0.001);
+        var salaryAfterSubmission = double.Parse(salaryLabel.Text, System.Globalization.CultureInfo.InvariantCulture);
+        var expectedSalary = 5000 * (1 + percentage / 100);
+        salaryAfterSubmission.Should().BeApproximately(expectedSalary, 0.001);
     }
+
     private bool IsElementPresent(By by)
     {
         try
